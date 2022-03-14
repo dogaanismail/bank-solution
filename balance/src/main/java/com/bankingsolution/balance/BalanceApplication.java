555f8fe1df82eb819c760f10fcd85415fcba3d3b
dogaanismail <ismaildogaan@gmail.com>
2022-03-14 19:33:27 +0300

    BankingSolution - Microservices and common projects have been added into the solution.

---
+++ balance/src/main/java/com/bankingsolution/balance/BalanceApplication.java
@@ -0,0 +1,13 @@
+package com.bankingsolution.balance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalanceApplication.class, args);
+	}
+
+}
---
+++ balance/src/test/java/com/bankingsolution/balance/BalanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankingsolution.balance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
