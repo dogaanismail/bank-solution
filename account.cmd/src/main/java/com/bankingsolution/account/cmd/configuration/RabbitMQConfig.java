c53ef41de685c84311ca7a10c0e5f5581dec2496
dogaanismail <ismaildogaan@gmail.com>
2022-12-18 21:59:29 +0300

    BankSolution - Add RabbitMQ ConnectionFactory bean.

--- account.cmd/src/main/java/com/bankingsolution/account/cmd/configuration/RabbitMQConfig.java
+++ account.cmd/src/main/java/com/bankingsolution/account/cmd/configuration/RabbitMQConfig.java
@@ -3,16 +3,31 @@ package com.bankingsolution.account.cmd.configuration;
 import com.bankingsolution.common.constants.Constants;
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableRabbit
 public class RabbitMQConfig {
+
+    @Value("${spring.rabbitmq.host}")
+    private String hostName;
+
+    @Value("${spring.rabbitmq.username}")
+    private String userName;
+
+    @Value("${spring.rabbitmq.password}")
+    private String password;
+
+    @Value("${spring.rabbitmq.port}")
+    private int port;
+
     @Bean
     public TopicExchange exchange() {
         return new TopicExchange(Constants.TOPIC_KEY);
@@ -71,6 +86,17 @@ public class RabbitMQConfig {
     @Bean
     public MessageConverter converter() {
         return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+        connectionFactory.setHost(this.hostName);
+        connectionFactory.setVirtualHost("/");
+        connectionFactory.setUsername(this.userName);
+        connectionFactory.setPassword(this.password);
+        connectionFactory.setPort(this.port);
+        return connectionFactory;
     }
 
     @Bean
